af1ab982e699ffffc55c9d9ba7e69047ac20f1d1
leonzhou <leon.zhou@xplusz.com>
2013-10-21 12:51:54 +0800

    Finish caluculate inventory without test

--- hot-deploy/sz-inventory/src/com/wjzpw/service/FindService.java
+++ hot-deploy/sz-inventory/src/com/wjzpw/service/FindService.java
@@ -1,9 +1,14 @@
 package com.wjzpw.service;
 
+import static org.ofbiz.base.util.UtilGenerics.checkMap;
+
+import java.util.List;
+import java.util.Map;
+
 import javolution.util.FastList;
 import javolution.util.FastMap;
+
 import org.apache.commons.lang.StringUtils;
-import org.ofbiz.base.util.UtilMisc;
 import org.ofbiz.entity.Delegator;
 import org.ofbiz.entity.GenericEntityException;
 import org.ofbiz.entity.GenericValue;
@@ -17,11 +22,6 @@ import org.ofbiz.service.DispatchContext;
 import org.ofbiz.service.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.util.List;
-import java.util.Map;
-
-import static org.ofbiz.base.util.UtilGenerics.checkMap;
 
 public class FindService {
 
@@ -47,9 +47,9 @@ public class FindService {
 
     /**
      * performFind
-     *
-     * This is a generic method that expects entity data affixed with special suffixes
-     * to indicate their purpose in formulating an SQL query statement.
+     * 
+     * This is a generic method that expects entity data affixed with special suffixes to indicate their purpose in
+     * formulating an SQL query statement.
      */
     public static Map<String, Object> performFindInventory(DispatchContext dctx, Map<String, ?> context) {
         Delegator delegator = dctx.getDelegator();
@@ -83,10 +83,13 @@ public class FindService {
         Integer listSize = null;
         try {
             int start = viewIndex.intValue() * viewSize.intValue();
-            EntityListIterator result = delegator.findListIteratorByCondition(inventoryView, condition, null, null, null, null);
+            EntityListIterator result = delegator.findListIteratorByCondition(inventoryView, condition, null, null,
+                    null, null);
             valueList = result.getPartialList(start, viewSize);
             listSize = result.getResultsSizeAfterPartialList();
             result.close();
+            // Calculate inventory
+            valueList = calculateInventory(delegator, valueList, condition);
         } catch (GenericEntityException e) {
             logger.error(e.getMessage(), e);
         }
@@ -94,9 +97,42 @@ public class FindService {
         Map<String, Object> results = ServiceUtil.returnSuccess();
         results.put("list", valueList);
         results.put("listSize", listSize);
-//        results.put("queryString", prepareResult.get("queryString"));
-//        results.put("queryStringMap", prepareResult.get("queryStringMap"));
+        // results.put("queryString", prepareResult.get("queryString"));
+        // results.put("queryStringMap", prepareResult.get("queryStringMap"));
         return results;
+    }
+
+    /**
+     * Calculate inventory by total input minus total output
+     */
+    private static List<GenericValue> calculateInventory(Delegator delegator, List<GenericValue> valueList, EntityCondition condition)
+            throws GenericEntityException {
+        // TODO optimize this function in future
+        // Query
+        DynamicViewEntity outputView = new DynamicViewEntity();
+        outputView.addMemberEntity("IO", "InventoryOutput");
+        outputView.addAlias("IO", "superiorNumber", "superiorNumber", null, null, null, "sum");
+        outputView.addAlias("IO", "gradeANumber", "gradeANumber", null, null, null, "sum");
+        outputView.addAlias("IO", "gradeBNumber", "gradeBNumber", null, null, null, "sum");
+        outputView.addAlias("IO", "productId", null, null, null, true, null);
+        outputView.addAlias("IO", "batchNoId", null, null, null, true, null);
+        EntityListIterator result = delegator
+                .findListIteratorByCondition(outputView, condition, null, null, null, null);
+        List<GenericValue> outputList = result.getCompleteList();
+
+        // Calculate
+        for (GenericValue value : valueList) {
+            for (GenericValue outputValue : outputList) {
+                if (value.get("productId") != null && value.get("productId").equals(outputValue.get("productId"))
+                        && value.get("batchNoId") != null
+                        && value.get("batchNoId").equals(outputValue.get("batchNoId"))) {
+                    value.set("superiorNumber", value.getDouble("superiorNumber") - outputValue.getDouble("superiorNumber"));
+                    value.set("gradeANumber", value.getDouble("gradeANumber") - outputValue.getDouble("gradeANumber"));
+                    value.set("gradeBNumber", value.getDouble("gradeBNumber") - outputValue.getDouble("gradeBNumber"));
+                }
+            }
+        }
+        return valueList;
     }
 
 }
